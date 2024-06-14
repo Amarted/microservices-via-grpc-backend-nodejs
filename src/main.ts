a3d78e86c13482ccfe9fd4ef17e0755de51ae055
DemnArctis <demnarctis@gmail.com>
2024-06-14 13:09:40 +0300

    Modified CORS activation.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './modules/application/application.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ApplicationModule, {cors: true});
+  const app = await NestFactory.create(ApplicationModule);
+  app.enableCors();
   app.enableShutdownHooks();
   await app.listen(3000);
 }
