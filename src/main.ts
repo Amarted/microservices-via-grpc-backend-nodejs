36c51efa25c0b2fcb774d1a586f77ee80738dd5d
DemnArctis <demnarctis@gmail.com>
2024-06-14 13:08:50 +0300

    Enable shutdown hooks to execute the ORM shutdown logic.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { ApplicationModule } from './modules/application/application.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule, {cors: true});
+  app.enableShutdownHooks();
   await app.listen(3000);
 }
 bootstrap();
